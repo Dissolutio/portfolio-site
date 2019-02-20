fc0e945d281120d68886617fc8ff47eaaa0de673
John Moen <entity.john@gmail.com>
2019-02-20 01:06:10 -0600

    update config for rollover plugins and site data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`
+    title: "Scatterbeam Web Development Dojo",
+    author: "John Moen",
+    description:
+      "A blog about web development and trying to get my first front-end developer job",
+    siteUrl: "https://www.scatterbeam.com"
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -14,13 +16,58 @@ module.exports = {
         path: `${__dirname}/src/assets`
       }
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`
+      }
+    },
+
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false
+            }
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590
+            }
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`
+            }
+          },
+          "gatsby-remark-prismjs",
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-smartypants"
+        ]
+      }
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `${process.env.GOOGLE_ANALYTICS_TRACKING_ID}`
+      }
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Scatterbeam Web Development Dojo`,
+        short_name: `Scatterbeam`,
         start_url: `/`,
         background_color: `#e4572e`,
         theme_color: `#6fffe9`,
@@ -32,6 +79,12 @@ module.exports = {
       resolve: "gatsby-plugin-typography",
       options: {
         pathToConfigModule: "src/utilities/typography"
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`monoton`]
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
