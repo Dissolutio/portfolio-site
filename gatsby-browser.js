2ad0b12e38e8befe88b8972d0e90ad03eeb124e4
John Moen <entity.john@gmail.com>
2019-02-20 01:07:57 -0600

    include prismjs theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import React from "react";
-
 import { BlogThemeProvider } from "./src/BlogThemeContext";
+require("prismjs/themes/prism-solarizedlight.css");
 
 export const wrapRootElement = ({ element }) => (
   <BlogThemeProvider>{element}</BlogThemeProvider>
