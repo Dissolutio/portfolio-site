674f4ba64ddbf8bcebad20255c8bb4a534227be6
John Moen <entity.john@gmail.com>
2019-02-27 13:15:07 -0600

    refactor typography config file

--- src/utilities/typography.js
+++ src/utilities/typography.js
@@ -3,11 +3,6 @@ import anonymousTheme from "typography-theme-anonymous";
 
 const typography = new Typography(anonymousTheme);
 
-// Hot reload typography in development.
-if (process.env.NODE_ENV !== "production") {
-  typography.injectStyles();
-}
-
-export default typography;
 export const rhythm = typography.rhythm;
 export const scale = typography.scale;
+export default typography;
