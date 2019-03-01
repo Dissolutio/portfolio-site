0e8fe0fd75fca80bf5ae8367458c9e497d328eaf
John Moen <entity.john@gmail.com>
2019-03-01 10:57:41 -0600

    remove unneeded HOC

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,1 @@
-import React from "react";
-import { BlogThemeProvider } from "./src/BlogThemeContext";
 require("prismjs/themes/prism-solarizedlight.css");
-
-export const wrapRootElement = ({ element }) => (
-  <BlogThemeProvider>{element}</BlogThemeProvider>
-);
