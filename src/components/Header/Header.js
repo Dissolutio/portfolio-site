0e414300f34d9c22b4cf67c09375d5c1bf59b78f
John Moen <entity.john@gmail.com>
2019-03-03 03:49:45 -0600

    Extract and create HeaderAvatar from Header, clean and refactor both

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,15 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "gatsby";
 import Img from "gatsby-image";
-import { ProfilePic } from "./Avatar";
+import HeaderAvatar from "./HeaderAvatar";
 import MainNav from "./MainNav";
 
 const Header = ({ data }) => (
   <HeaderContainer>
     <HeaderContent>
-      <HeaderAvatar to="/">
-        <ProfilePic />
-      </HeaderAvatar>
+      <HeaderAvatar fluid={data.profilePic.childImageSharp.fluid} />
       <h1>John Moen</h1>
-      <h2>Frontend Developmer</h2>
-      <ul className="icons">
-        <li>
-          <a href="https://github.com/Dissolutio" className="icon fa-github">
-            <span className="label">Github</span>
-          </a>
-        </li>
-      </ul>
+      <h2>Frontend Developer</h2>
       <MainNav />
     </HeaderContent>
     <HeroBackgroundDiv>
@@ -34,6 +24,7 @@ const HeaderContent = styled.div`
   font-size: 1rem;
   margin: 0 0 2rem 0;
   width: 100%;
+  height: 100%;
   padding: 1rem;
   h1 {
     font-size: 1.6rem;
@@ -42,25 +33,14 @@ const HeaderContent = styled.div`
     font-size: 1.4rem;
   }
 `;
-
 const HeaderContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-end;
-  justify-content: space-between;
   text-align: right;
   padding: 2rem;
-  color: #ffffff;
+  color: ${props => props.theme.color.white};
   height: 100%;
-  z-index: 10;
-  opacity: 0.9;
-  background-color: #1f1815;
-  background-attachment: scroll, fixed;
-  background-image: url("images/overlay.png"),
-    url("../images/jason-leung-714414-unsplash.jpg");
-  background-position: top left, left;
-  background-repeat: repeat, no-repeat;
-  background-size: auto, auto 120%;
+  background-color: rgba(47, 47, 47, 0.5);
 `;
 const HeroBackgroundDiv = styled.div`
   position: absolute;
@@ -68,29 +48,11 @@ const HeroBackgroundDiv = styled.div`
   top: 0;
   width: 100%;
   height: 100%;
-  overflow: hidden;
   z-index: -1;
 `;
 const HeroBackgroundImg = styled(Img)`
   width: 100%;
   height: 100%;
-  opacity: 0.9;
-`;
-
-const HeaderAvatar = styled(Link)`
-  position: relative;
-  height: 100px;
-  width: 100px;
-  border: 0;
-  outline: none;
-  transition: color 0.2s ease-in-out, border-color 0.2s ease-in-out;
-  text-decoration: none;
-  margin: 0 0 1em 0;
-  border: 0;
-  display: inline-block;
-  img {
-    border-radius: 50%;
-  }
 `;
 
 export default Header;
---
+++ src/components/Header/HeaderAvatar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+import Img from "gatsby-image";
+import { Link } from "gatsby";
+
+const HeaderAvatar = ({ fluid }) => (
+  <HeaderAvatarContainer>
+    <AvatarOverlay />
+    <Img fluid={fluid} />
+  </HeaderAvatarContainer>
+);
+const HeaderAvatarContainer = styled(Link)`
+  position: relative;
+  height: 100px;
+  width: 100px;
+  border: 0;
+  outline: none;
+  transition: color 0.2s ease-in-out, border-color 0.2s ease-in-out;
+  text-decoration: none;
+  margin: 0 0 1em 0;
+  border: 0;
+  display: inline-block;
+  box-shadow: none;
+  img {
+    border-radius: 50%;
+  }
+`;
+const AvatarOverlay = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(47, 47, 47, 0.2);
+  border-radius: 50%;
+  z-index: 10;
+`;
+export default HeaderAvatar;
