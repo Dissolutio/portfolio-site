3093049f0183cd6eb736ddd8db1ef1db4bdb00af
John Moen <entity.john@gmail.com>
2019-03-02 22:19:43 -0600

    change typography method, following docs

--- src/utilities/typography.js
+++ src/utilities/typography.js
@@ -1,10 +1,9 @@
 import Typography from "typography";
 import anonymousTheme from "typography-theme-anonymous";
 
-const typography = new Typography({
-  ...anonymousTheme,
-  baseFontSize: "16px"
-});
+anonymousTheme.baseFontSize = "16px";
+
+const typography = new Typography(anonymousTheme);
 
 export const rhythm = typography.rhythm;
 export const scale = typography.scale;
