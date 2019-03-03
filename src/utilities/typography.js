dc6a98e08916e5864052d502f0aea2e528bcd9b5
John Moen <entity.john@gmail.com>
2019-03-03 03:49:04 -0600

    change typography config

--- src/utilities/typography.js
+++ src/utilities/typography.js
@@ -2,6 +2,7 @@ import Typography from "typography";
 import anonymousTheme from "typography-theme-anonymous";
 
 anonymousTheme.baseFontSize = "16px";
+anonymousTheme.scale = "1.8";
 
 const typography = new Typography(anonymousTheme);
 
