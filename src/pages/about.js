dd98a68ff2df0dcd8d05f3b6ff8bbe808e64a75a
John Moen <entity.john@gmail.com>
2019-03-07 21:35:58 -0600

    rewrite about me

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,35 +9,27 @@ const SecondPage = ({ data }) => (
   <StrataLayout data={data}>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>About Me</h1>
-    <p>My name is John, I am mostly curious and thankful.</p>
     <p>
-      In January 2018 is when I decided I was going to learn how to code and get
-      a developer job or build a freelance business. It's been a lot of work
-      since then, but work with passion behind it can really make the time fly.
+      I am a self-taught programmer, and eventhough I'm late to the party, let
+      me tell you, I am having an incredible time.
     </p>
+    <h2>Modern web development offers powerful possibilities</h2>
+    An elegant tool can empower human and tribe alike. And it can bring joy
+    through its very use. The apps to be made will be part of how we as humanity
+    interface with one another and our accumulated knowledge.
     <p>
-      My main tools lately are Node.js and React.js, but the modern toolset is
-      rapidly expanding, and I am thrilled to perpetually learn new techniques
-      and architechtures. The wild world of software is a playground for my
-      mind.
+      <h2>The challenges facing humanity are great</h2>
+      There are helpers around the world who are drawn to a similar vision.
+      These people are doing urgent work, and they need the power of modern
+      software tools.
     </p>
-    <p>
-      The challenges facing humanity, and the individual human, are great. There
-      are helpers around the world who are drawn to a bigger picture of the
-      future, a positive vision. They are all around, and they are doing
-      valuable, urgent work on many fronts to construct a sustainable and
-      equitable global human civilization.
-    </p>
-    <p>
-      All these helpers, with all their renewable energy, ecological
-      restoration, social work, holistic health and organizational transparency
-      , need powerful web solutions and are often short on funds to compete for
-      cutting edge tech. Gee, who's gonna build those?
-    </p>
+    <h2>UI Development for a more enjoyable spacetime</h2>
     <p>
       There's a lot of work to do, and it makes my life so easy lucky and free
       to know my place in the struggle.
     </p>
+    <h2>My primary tools and techniques utilize JavaScript / React</h2>
+    <p>Both me and the modern toolset are becoming sharper every single day!</p>
   </StrataLayout>
 );
 
