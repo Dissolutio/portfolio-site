e9188ba275969fb17305d22463bf0b7e1ad0ed29
John Moen <entity.john@gmail.com>
2019-06-11 00:40:20 -0500

    remove hire me page

--- src/components/Header/MainNav.js
+++ src/components/Header/MainNav.js
@@ -1,41 +1,38 @@
-import React, { Component } from "react";
-import { Link } from "gatsby";
-import styled from "styled-components";
+import React, { Component } from 'react'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
 
 export default class MainNav extends Component {
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li>
-            <StyledLink to="/">Home</StyledLink>
-          </li>
-          <li>
-            <StyledLink to="/about">About</StyledLink>
-          </li>
-          <li>
-            <StyledLink to="/blog">Blog</StyledLink>
-          </li>
-          <li>
-            <StyledLink to="/hire-me">Hire Me</StyledLink>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
+	render() {
+		return (
+			<nav>
+				<ul>
+					<li>
+						<StyledLink to="/">Home</StyledLink>
+					</li>
+					<li>
+						<StyledLink to="/about">About</StyledLink>
+					</li>
+					<li>
+						<StyledLink to="/blog">Blog</StyledLink>
+					</li>
+				</ul>
+			</nav>
+		)
+	}
 }
 const StyledLink = styled(Link)`
-  text-decoration: none;
-  font-size: 1.3rem;
-  color: ${props => props.theme.color.blue5};
-  border: none;
-  box-shadow: none;
-  border-bottom: 1px solid ${props => props.theme.color.blue5};
-  &:visited {
-    color: ${props => props.theme.color.blue5};
-  }
-  &:hover {
-    border-bottom: 3px dashed ${props => props.theme.color.blue5};
-    color: ${props => props.theme.color.blue5};
-  }
-`;
+	text-decoration: none;
+	font-size: 1.3rem;
+	color: ${props => props.theme.color.blue5};
+	border: none;
+	box-shadow: none;
+	border-bottom: 1px solid ${props => props.theme.color.blue5};
+	&:visited {
+		color: ${props => props.theme.color.blue5};
+	}
+	&:hover {
+		border-bottom: 3px dashed ${props => props.theme.color.blue5};
+		color: ${props => props.theme.color.blue5};
+	}
+`
